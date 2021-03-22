66789b5af6e549cb4c7268eb998b7e6d280c0108
Ismael-Rods <irodriguez5@miuandes.cl>
2021-03-22 17:20:19 -0300

    Primeras funciones

--- OneDrive/Documents/GitHub/memorice.py
+++ OneDrive/Documents/GitHub/memorice.py
@@ -1,10 +1,41 @@
-n= int(input('Ingrese numero de cartas a jugar:'))
 from random import shuffle
-i=1
-lista=[]
-for g in range(n):
-    lista.append(i)
-    lista.append(i)
-    i+=1
-shuffle(lista)
-print(lista)
+from tabulate import tabulate
+def shuffle_card_list(n):
+    i=1
+    list=[]
+    for g in range(n):
+        list.append(i)
+        list.append(i)
+        i+=1
+    shuffle(list)
+    return(list)
+
+def cens_list(n):
+    list=[]
+    for g in range(n):
+        list.append('*')
+        list.append('*')
+    return list
+def generate_board(list1,row_amount):
+    l=[]
+    x=0
+    y=10
+    u=0
+    for i in range(row_amount):
+        g=list1[x:y]
+        g.insert(0,u)
+        l.append(g)
+        u+=1
+        x+=10
+        y+=10
+    print(tabulate(l,headers=['r/c ','1','2','3','4','5','6','7','8','9','10']))
+
+n= int(input('Enter number of cards to play:'))
+row_amount=int(input('Enter the number of rows on your board, thinking about the number of pairs and consider 10 columns\nFor Example: 20 pairs = 4 rows: '))
+
+#P1=0
+#P2=0
+#print(shuffle_card_list(20))
+print(generate_board(shuffle_card_list(n),row_amount))
+
+print(generate_board(cens_list(n),row_amount))
