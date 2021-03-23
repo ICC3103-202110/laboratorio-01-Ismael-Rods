31850206c7f36d7ae46bee7d4abc18883d74c0eb
Ismael-Rods <irodriguez5@miuandes.cl>
2021-03-22 21:16:19 -0300

    Game completed, turn functions and while loop ended

--- OneDrive/Documents/GitHub/memorice.py
+++ OneDrive/Documents/GitHub/memorice.py
@@ -1,5 +1,11 @@
 from random import shuffle
 from tabulate import tabulate
+def empty_board_list(n):
+    list=[]
+    for g in range(n):
+        list.append('')
+        list.append('')
+    return list
 def shuffle_card_list(n):
     i=1
     list=[]
@@ -30,36 +36,50 @@ def generate_board(list1,row_amount):
         y+=10
     print(tabulate(l,headers=['r/c ','0','1','2','3','4','5','6','7','8','9']))
 
-def turno_p1():
+def turn_p1(P1):
     print('Player 1 turn!')
     generate_board(game_board,row_amount)
     print('Select a card!')
-    x=int(input('Enter coordinates (x,y)!\nFor example: (1,1) = 11 : '))
+    x=int(input('Enter coordinates (row,colum)!\nFor example: (2,6) = 26 : '))
     game_board[x]=deck[x]
     generate_board(game_board,row_amount)
     print('Select the second card!')
-    x2=int(input('Enter coordinates (x,y)!\nFor example: (1,1) = 11 : '))
+    x2=int(input('Enter coordinates (row,colum)!\nFor example: (2,6) = 26 : '))
     game_board[x2]=deck[x2]
     generate_board(game_board,row_amount)
     if deck[x]==deck[x2]:
-        True
+        game_board[x]=''
+        game_board[x2]=''
+        print('Good! You scored! Pick a card again! ')
+        P1+=1
+        return (True,True)
     else:
-        False
-def turno_p2():
+        game_board[x]='*'
+        game_board[x2]='*'
+        print('¡Oh! You have failed!')
+        return False
+def turn_p2(P2):
     print('Player 2 turn!')
     generate_board(game_board,row_amount)
     print('Select a card!')
-    x=int(input('Enter coordinates (x,y)!\nFor example: (1,1) = 11 : '))
+    x=int(input('Enter coordinates (row,colum)!\nFor example: (2,6) = 26 : '))
     game_board[x]=deck[x]
     generate_board(game_board,row_amount)
     print('Select the second card!')
-    x2=int(input('Enter coordinates (x,y)!\nFor example: (1,1) = 11 : '))
+    x2=int(input('Enter coordinates (row,colum)!\nFor example: (2,6) = 26 : '))
     game_board[x2]=deck[x2]
     generate_board(game_board,row_amount)
     if deck[x]==deck[x2]:
-        True
+        game_board[x]=''
+        game_board[x2]=''
+        print('Good! You scored! Pick a card again! ')
+        P2+=1
+        return (False,False)
     else:
-        False
+        game_board[x]='*'
+        game_board[x2]='*'
+        print('¡Oh! You have failed!')
+        return True
 
 print('Welcome!')
 P1=0
@@ -69,11 +89,28 @@ row_amount=int(input('Enter the number of rows on your board, thinking about the
 deck=shuffle_card_list(n)
 generate_board(deck,row_amount)
 print('\n')
-game_board=cens_list(n)
+game_board=cens_list(n) 
+finish_list=empty_board_list(n)
+GranZ=turn_p1(P1)
 
-list=[]
-for g in range(n):
-    list.append('')
-    list.append('')
-generate_board(list,row_amount)
-print(list)
+while game_board!=finish_list:
+    if GranZ==(True,True):
+        P1+=1
+        print('P1=',P1)
+        GranZ=turn_p1(P1)
+    elif GranZ==False:
+        print('P2=',P2)
+        GranZ=turn_p2(P2)
+    elif GranZ==(False,False):
+        P2+=1
+        print('P2=',P2)
+        GranZ=turn_p2(P2)
+    elif GranZ==True:
+        print('P1=',P1)
+        GranZ=turn_p1(P1)
+if P1>P2:
+    print('P1 wins!')    
+elif P1<P2:
+    print('P2 wins!')
+elif P1==P2:
+    print('Tie!')
